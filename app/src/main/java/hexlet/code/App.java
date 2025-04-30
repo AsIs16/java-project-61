4713d9c362c6e98272f835aa771929417b7955bd
AsIs16 <asabaliev.isalm77@mail.ru>
2025-04-30 12:09:15 +0300

    First intermediate commit

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,9 +1,20 @@
 package hexlet.code;
 
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) {
-        System.out.println("Welcome to the Brain Games!");
+        System.out.println("Please enter the game number and press Enter.\n1 - Greet\n0 - Exit");
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Your choice: ");
+        int userChoose = sc.nextInt();
+        if (userChoose == 1) {
+            System.out.println("Welcome to the Brain Games!");
+            Cli.greetUser();
+        } else {
+            return;
+        }
 
-        Cli.greetUser();
+//        Cli.greetUser();
     }
 }
