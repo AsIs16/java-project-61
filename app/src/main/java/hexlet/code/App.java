1328273155e442ab6adc0c3dd31b7dca7c3d6a97
AsIs16 <asabaliev.isalm77@mail.ru>
2025-05-01 21:45:59 +0300

    add Welcome

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -8,6 +8,7 @@ public class App {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
+        System.out.println("Welcome to the Brain Games!");
         System.out.println("Please enter the game number and press Enter.\n1 - Greet\n2 - Even\n3 - Calc\n4 - GCD\n5 - Progression\n6 - Prime\n0 - Exit");
         System.out.print("Your choice: ");
         int userChoose = sc.nextInt();
